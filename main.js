1e843fff7b0134422e3744426d49640d84b2fc18
r0930514 <r0930514@gmail.com>
2023-12-13 13:12:39 +0800

    static 資料夾存取設定

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import { Express } from "express";
+import express from "express"
+const app = express()
+
+app.use('/static', express.static("./static"))
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello World")
+})
+
+app.listen(3000, ()=>{
+    console.log("Hello")
+})

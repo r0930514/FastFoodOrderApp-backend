013b870dc5938fdad2bed4fa65b5c4967dd0b477
r0930514 <r0930514@gmail.com>
2023-12-14 11:28:43 +0800

    修正無法存取檔案

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ import express from "express"
 import indexRouter from "./router/index.js"
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use('/debug', indexRouter)
+
 
 app.listen(3000, ()=>{
     console.log("Hello")
--- router/index.js
+++ router/index.js
@@ -2,6 +2,12 @@ import express from "express"
 import { Router } from "express"
 const indexRouter = Router()
 
-indexRouter.use('/static', express.static('../static'))
+indexRouter.use('/static', express.static('./static'))
+
+indexRouter.post('/user', (req, res)=>{
+    console.log(req.body);
+    res.sendStatus(200)
+})
+
 
 export default indexRouter
